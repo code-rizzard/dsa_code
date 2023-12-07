bc3aa3c94e4c05d7417c337ca881d3ae07b340b7
rizzard <62689803+code-rizzard@users.noreply.github.com>
2023-12-08 07:24:22 +0800

    Removed commnets

--- calcu.cpp
+++ calcu.cpp
@@ -6,7 +6,6 @@ int main() {
     char operation;
     double num1, num2, result;
 
-    // Input operation and numbers
     cout << "Enter operation (+, -, *, /): ";
     cin >> operation;
 
@@ -16,7 +15,6 @@ int main() {
     cout << "Enter second number: ";
     cin >> num2;
 
-    // Perform the selected operation using a switch statement
     switch (operation) {
         case '+':
             result = num1 + num2;
@@ -28,17 +26,16 @@ int main() {
             result = num1 * num2;
             break;
         case '/':
-            // Check for division by zero
             if (num2 != 0) {
                 result = num1 / num2;
             } else {
                 cout << "Error: Division by zero is undefined." << endl;
-                return 1;  // Exit with an error code
+                break;
             }
             break;
         default:
             cout << "Error: Invalid operation." << endl;
-            return 1;  // Exit with an error code
+            break;
     }
 
     // Output the result
